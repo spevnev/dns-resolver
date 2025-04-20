9b1a0a3a0b3b3100d4b76df92bb54e441149628b
spevnev <spevnev16@gmail.com>
2025-04-19 22:33:05 -0400

    Improve command line argument parsing

---
+++ src/args.c
@@ -0,0 +1,228 @@
+#include "args.h"
+#include <assert.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include "error.h"
+
+typedef enum {
+    TYPE_BOOL,
+    TYPE_LONG,
+    TYPE_STR,
+} OptionType;
+
+typedef union {
+    bool bool_;
+    long long_;
+    const char *str;
+} OptionValue;
+
+typedef struct {
+    bool show_default;
+    char short_name;
+    const char *name;
+    const char *description;
+    OptionType type;
+    OptionValue default_value;
+    OptionValue value;
+} Option;
+
+typedef struct {
+    uint32_t args_length;
+    char **args;
+    const char *program;
+    uint32_t capacity;
+    uint32_t length;
+    Option *data;
+} Options;
+
+static Options options = {0};
+
+#define SHIFT_ARGS() (assert(argc > 0), argc--, *(argv++))
+
+static void parse_arg(Option *option, char *arg_value) {
+    switch (option->type) {
+        case TYPE_BOOL:
+            if (strcmp(arg_value, "true") == 0) {
+                option->value.bool_ = true;
+            } else if (strcmp(arg_value, "false") == 0) {
+                option->value.bool_ = false;
+            } else {
+                ERROR("Invalid option value, expected a boolean but found \"%s\"", arg_value);
+            }
+            break;
+        case TYPE_LONG: {
+            char *end = NULL;
+            option->value.long_ = strtol(arg_value, &end, 10);
+            if (*end != '\0') ERROR("Invalid option value, expected a number but found \"%s\"", arg_value);
+        } break;
+        case TYPE_STR: option->value.str = arg_value; break;
+    }
+}
+
+static Option *new_option(char short_name, const char *name, const char *description, bool show_default) {
+    if (options.length >= options.capacity) {
+        options.capacity = options.capacity == 0 ? 16 : options.capacity * 2;
+        options.data = realloc(options.data, options.capacity * sizeof(*options.data));
+        if (options.data == NULL) OUT_OF_MEMORY();
+    }
+    Option *option = &options.data[options.length++];
+    option->short_name = short_name;
+    option->name = name;
+    option->description = description;
+    option->show_default = show_default;
+    return option;
+}
+
+bool *option_bool(char short_name, const char *name, const char *description, bool show_default, bool default_value) {
+    Option *option = new_option(short_name, name, description, show_default);
+    option->type = TYPE_BOOL;
+    option->default_value.bool_ = option->value.bool_ = default_value;
+    return &option->value.bool_;
+}
+
+long *option_long(char short_name, const char *name, const char *description, bool show_default, long default_value) {
+    Option *option = new_option(short_name, name, description, show_default);
+    option->type = TYPE_LONG;
+    option->default_value.long_ = option->value.long_ = default_value;
+    return &option->value.long_;
+}
+
+const char **option_str(char short_name, const char *name, const char *description, bool show_default,
+                        const char *default_value) {
+    Option *option = new_option(short_name, name, description, show_default);
+    option->type = TYPE_STR;
+    option->default_value.str = option->value.str = default_value;
+    return &option->value.str;
+}
+
+char *parse_args(int argc, char **argv) {
+    options.args = argv;
+
+    if (argc == 0) ERROR("Invalid arguments");
+    char *program = SHIFT_ARGS();
+
+    while (argc > 0) {
+        char *arg = SHIFT_ARGS();
+        size_t arg_len = strlen(arg);
+
+        if (arg_len >= 2 && arg[0] == '-' && arg[1] != '-') {
+            bool found = false;
+            for (uint32_t i = 0; i < options.length; i++) {
+                Option *option = &options.data[i];
+                if (arg[1] != option->short_name) continue;
+
+                // -O=v or -Ov
+                if (arg[2] != '\0') {
+                    if (arg[2] == '=') arg++;
+                    parse_arg(option, arg + 2);
+                    found = true;
+                    break;
+                }
+
+                // -O v
+                if (option->type != TYPE_BOOL) {
+                    if (argc == 0) ERROR("Option \"%s\" requires argument", arg);
+                    parse_arg(option, SHIFT_ARGS());
+                    found = true;
+                    break;
+                }
+
+                if (argc > 0 && (strcmp(argv[0], "false") == 0 || strcmp(argv[0], "true") == 0)) {
+                    // -O false or -O true
+                    parse_arg(option, SHIFT_ARGS());
+                } else {
+                    // -O
+                    option->value.bool_ = true;
+                }
+                found = true;
+                break;
+            }
+            if (!found) ERROR("Invalid option \"%s\"", arg);
+        } else if (arg_len >= 3 && arg[0] == '-' && arg[1] == '-') {
+            char *arg_name = arg + 2;
+            size_t arg_name_len = arg_len - 2;
+            bool found = false;
+            for (uint32_t i = 0; i < options.length; i++) {
+                Option *option = &options.data[i];
+                size_t name_len = strlen(option->name);
+                if (arg_name_len < name_len || strncmp(arg_name, option->name, name_len) != 0) continue;
+
+                // --opt=v
+                if (arg_name[name_len] == '=') {
+                    parse_arg(option, arg_name + name_len + 1);
+                    found = true;
+                    break;
+                }
+
+                // Different option with same prefix or invalid option.
+                if (arg_name[name_len] != '\0') continue;
+
+                // --opt v
+                if (option->type != TYPE_BOOL) {
+                    if (argc == 0) ERROR("Option \"%s\" requires argument", arg);
+                    parse_arg(option, SHIFT_ARGS());
+                    found = true;
+                    break;
+                }
+
+                if (argc > 0 && (strcmp(argv[0], "false") == 0 || strcmp(argv[0], "true") == 0)) {
+                    // --opt false or --opt true
+                    parse_arg(option, SHIFT_ARGS());
+                } else {
+                    // --opt
+                    option->value.bool_ = true;
+                }
+                found = true;
+                break;
+            }
+            if (!found) ERROR("Invalid option \"%s\"", arg);
+        } else {
+            // Move all non-option arguments towards beginning of argv.
+            options.args[options.args_length++] = arg;
+        }
+    }
+
+    return program;
+}
+
+bool has_next_arg(void) { return options.args_length > 0; }
+
+char *next_arg(void) {
+    assert(options.args_length > 0);
+    options.args_length--;
+    return *(options.args++);
+}
+
+void print_options(void) {
+    size_t max_name_len = 0;
+    for (uint32_t i = 0; i < options.length; i++) {
+        size_t name_length = strlen(options.data[i].name);
+        if (name_length > max_name_len) max_name_len = name_length;
+    }
+
+    for (uint32_t i = 0; i < options.length; i++) {
+        Option *option = &options.data[i];
+
+        if (option->short_name == '\0') {
+            printf("     ");
+        } else {
+            printf("  -%c,", option->short_name);
+        }
+
+        printf(" --%-*s  %s", (int) max_name_len, option->name, option->description);
+
+        if (!option->show_default) {
+            printf("\n");
+            continue;
+        }
+
+        printf(", default=");
+        switch (option->type) {
+            case TYPE_BOOL: printf(option->default_value.bool_ ? "true" : "false"); break;
+            case TYPE_LONG: printf("%ld", option->default_value.long_); break;
+            case TYPE_STR:  printf("%s", option->default_value.str); break;
+        }
+        printf("\n");
+    }
+}
--- src/main.c
+++ src/main.c
@@ -4,70 +4,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "args.h"
 #include "dns.h"
 #include "error.h"
 #include "resolve.h"
 
 static const char *ROOT_NAMESERVER_IP = "198.41.0.4";  // a.root-servers.net
 
-#define SHIFT_ARGS() (assert(argc > 0), argc--, *(argv++))
-
 static void usage(const char *program) {
-    printf("Usage:\n");
-    printf("  %s domain\n", program);
+    printf("Usage: %s domain\n", program);
     printf("Options:\n");
-    printf("  -h, --help      - show this message\n");
-    printf("  -s, --server    - specify nameserver IP address\n");
-    printf("  -t, --type      - specify query type\n");
-    printf("  -T, --timeout   - timeout in seconds\n");
-    printf("  --no-recursion  - set Recursion Desired to false\n");
+    print_options();
 }
 
 int main(int argc, char **argv) {
-    if (argc == 0) ERROR("Invalid arguments");
-    const char *program = SHIFT_ARGS();
+    bool *help = option_bool('h', "help", "show this message", false, false);
+    const char **nameserver_ip = option_str('s', "server", "specify nameserver IP address", false, ROOT_NAMESERVER_IP);
+    const char **qtype_str = option_str('t', "type", "specify query type", true, "A");
+    long *timeout_sec = option_long('T', "timeout", "timeout in seconds", true, 10);
+    bool *recursion_desired = option_bool(0, "recurse", "set Recursion Desired", true, true);
 
-    const char *nameserver_ip = ROOT_NAMESERVER_IP;
-    uint16_t qtype = TYPE_A;
-    int timeout_sec = 10;
-    bool recursion_desired = true;
-    char *domain = NULL;
-    while (argc > 0) {
-        char *arg = SHIFT_ARGS();
+    const char *program = parse_args(argc, argv);
 
-        if (strcmp(arg, "-h") == 0 || strcmp(arg, "--help") == 0) {
-            usage(program);
-            return EXIT_SUCCESS;
-        } else if (strcmp(arg, "-s") == 0 || strcmp(arg, "--server") == 0) {
-            if (argc == 0) ERROR("Expected IP address of nameserver after %s", arg);
-            nameserver_ip = SHIFT_ARGS();
-        } else if (strcmp(arg, "-t") == 0 || strcmp(arg, "--type") == 0) {
-            if (argc == 0) ERROR("Expected query type after %s", arg);
-            qtype = get_qtype(SHIFT_ARGS());
-        } else if (strcmp(arg, "-T") == 0 || strcmp(arg, "--timeout") == 0) {
-            if (argc == 0) ERROR("Expected timeout after %s", arg);
-            const char *timeout_arg = SHIFT_ARGS();
-
-            char *end = NULL;
-            timeout_sec = strtol(timeout_arg, &end, 10);
-            if (*end != '\0' || timeout_sec <= 0) {
-                ERROR("Timeout must be a positive integer but found \"%s\"", timeout_arg);
-            }
-        } else if (strcmp(arg, "--no-recursion") == 0) {
-            recursion_desired = false;
-        } else {
-            // Unknown flag/option.
-            size_t arg_len = strlen(arg);
-            if (arg_len > 2 && arg[0] == '-' && arg[1] != '-') ERROR("Unknown option \"%s\"", arg);  // short option
-            if (arg_len > 3 && arg[0] == '-' && arg[1] == '-') ERROR("Unknown option \"%s\"", arg);  // long option
-
-            // Multiple arguments.
-            if (domain != NULL) ERROR("Expected one argument (domain) but found \"%s\" and \"%s\"", domain, arg);
-            domain = arg;
-        }
+    if (*help) {
+        usage(program);
+        return EXIT_SUCCESS;
     }
-    if (domain == NULL) ERROR("Invalid arguments, domain is not specified");
 
-    resolve(domain, nameserver_ip, qtype, timeout_sec, recursion_desired);
+    uint16_t qtype = get_qtype(*qtype_str);
+    if (*timeout_sec <= 0) ERROR("Timeout must be a positive integer");
+
+    if (!has_next_arg()) ERROR("Invalid arguments, domain is not specified");
+    char *domain = next_arg();
+    if (has_next_arg()) ERROR("Expected one argument (domain) but found \"%s\" and \"%s\"", domain, next_arg());
+
+    resolve(domain, *nameserver_ip, qtype, *timeout_sec, *recursion_desired);
     return EXIT_SUCCESS;
 }
